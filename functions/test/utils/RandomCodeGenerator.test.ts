22c8067d95f78c49790ee4dab8d1f686e38cfbcb
Eric Weidow <85724887+Studsministern@users.noreply.github.com>
2024-12-06 13:38:17 +0100

    refactor: RandomCodeGenerator tests

--- functions/test/utils/RandomCodeGenerator.test.ts
+++ functions/test/utils/RandomCodeGenerator.test.ts
@@ -2,45 +2,43 @@ import RandomCodeGenerator from "../../src/utils/RandomCodeGenerator";
 import {expect} from 'chai';
 
 
-describe('is alpha numeric', () => {
-
-    const isAlphaNumeric = RandomCodeGenerator['isAlphaNumeric'];
-
-    it('just numeric should fail', () => {
-        const result = isAlphaNumeric("1234567890");
-        expect(result).to.be.eq(false);
+describe('RandomCodeGenerator tests', () => {
+    describe('generate', () => {
+        it('should generate random code on normal operation', () => {
+            const generator = new RandomCodeGenerator(6);
+            const code = generator.generate();
+            expect(code).to.be.a('string');
+            expect(code.length).to.be.eq(6);
+        });
     });
 
-    it('just alpha should fail', () => {
-        const result = isAlphaNumeric("HELLO");
-        expect(result).to.be.eq(false);
+    describe("isAlphaNumeric", () => {
+        const isAlphaNumeric = RandomCodeGenerator["isAlphaNumeric"];
+        
+        it("should pass alphanumeric with both numbers and letters", () => {
+            const result = isAlphaNumeric("HELLO123");
+            expect(result).to.be.eq(true);
+        });
+
+        it("should fail with just numbers", () => {
+            const result = isAlphaNumeric("1234567890");
+            expect(result).to.be.eq(false);
+        });
+
+        it("should fail with just letters", () => {
+            const result = isAlphaNumeric("HELLO");
+            expect(result).to.be.eq(false);
+        });
+
+        it("should fail with special characters", () => {
+            const result = isAlphaNumeric("HELLO123!");
+            expect(result).to.be.eq(false);
+        });
+
+        it("should fail with empty string", () => {
+            const result = isAlphaNumeric("");
+            expect(result).to.be.eq(false);
+        });
     });
-
-    it ('should pass alphanumeric', () => {
-        const result = isAlphaNumeric("HELLO123");
-        expect(result).to.be.eq(true);
-    });
-
-    it ('should fail on special characters', () => {
-        const result = isAlphaNumeric("HELLO123!");
-        expect(result).to.be.eq(false);
-    });
-
-    it ('schould fail on empty string', () => {
-        const result = isAlphaNumeric("");
-        expect(result).to.be.eq(false);
-    });
-
-});
-
-describe('RandomCodeGenerator tests', function() {
-
-    it('should generate random code on normal operation', () => {
-        const generator = new RandomCodeGenerator(6);
-        const code = generator.generate();
-        expect(code).to.be.a('string');
-        expect(code.length).to.be.eq(6);
-    });
-
 });
 
