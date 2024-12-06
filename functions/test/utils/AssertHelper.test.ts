f2ca189693f6f449debcc680173bf7da0f55eb0f
Eric Weidow <85724887+Studsministern@users.noreply.github.com>
2024-12-06 12:55:29 +0100

    test: remove black-box tests

--- functions/test/utils/AssertHelper.test.ts
+++ functions/test/utils/AssertHelper.test.ts
@@ -6,30 +6,6 @@ describe('AssertHelper tests', () => {
         expect(() => notNull(null)).to.throw();
     });
 
-    it('should not throw when passing undefined to notNull', () => {
-        expect(() => notNull(undefined)).to.not.throw();
-    });
-
-    it('should not throw when passing false to notNull', () => {
-        expect(() => notNull(false)).to.not.throw();
-    });
-
-    it('should not throw when passing 0 to notNull', () => {
-        expect(() => notNull(0)).to.not.throw();
-    });
-    
-    it('should not throw when passing an empty string to notNull', () => {
-        expect(() => notNull("")).to.not.throw();
-    });
-
-    it('should not throw when passing an empty array to notNull', () => {     
-        expect(() => notNull([])).to.not.throw();
-    });
-
-    it('should not throw when passing the string "0" to notNull', () => {
-        expect(() => notNull("0")).to.not.throw();
-    });
-
     it('should not throw when passing 1 to notNull', () => {
         expect(() => notNull(1)).to.not.throw();
     });
